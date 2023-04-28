ee8090e166a29d46d9b9d426fbf118978e6d569b
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-28 12:43:40 -0500

    add solution

--- Validate_Subsequence/Validate_Subsequence_solution.py
+++ Validate_Subsequence/Validate_Subsequence_solution.py
@@ -1,2 +1,8 @@
 def isValidSubsequence(array, sequence):
-    return
+    pointer = 0
+    for ele in array:
+        if ele == sequence[pointer]:
+            pointer += 1
+        if pointer == len(sequence):
+            return True
+    return False
