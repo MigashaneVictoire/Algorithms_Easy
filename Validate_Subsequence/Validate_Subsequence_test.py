2f31dfa90e3d4ec7e3a00a7efad1815c43c0dff4
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-28 13:12:01 -0500

    add test case 1-10 --> failed tests

--- Validate_Subsequence/Validate_Subsequence_test.py
+++ Validate_Subsequence/Validate_Subsequence_test.py
@@ -7,5 +7,51 @@ class TestSolution(unittest.TestCase):
         sequence = [1, 6, -1, 10]
         self.assertTrue(isValidSubsequence(array, sequence))
 
+    def test_case_2(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [5, 1, 22, 25, 6, -1, 8, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_3(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [5, 1, 22, 6, -1, 8, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_4(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [22, 25, 6]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_5(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [1, 6, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_6(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [5, 1, 22, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_7(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [5, -1, 8, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_8(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [25]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_9(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [1, 1, 1]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+    def test_case_10(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
+        self.assertTrue(isValidSubsequence(array, sequence))
+
+ 
 if __name__ == "__main__":
     unittest.main()
