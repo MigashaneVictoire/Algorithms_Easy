482ff4164266e227382c6e9de134b6e6b17e092d
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-29 13:07:03 -0500

    add test case 2-12

--- Two_Number_Sum/tests.py
+++ Two_Number_Sum/tests.py
@@ -9,6 +9,83 @@ class TestSolution(unittest.TestCase):
         actual = twoNumberSum(array, targetSum)
         self.assertEqual(expected, actual)
 
+    def test_case_2(self):
+        array = [4, 6]
+        targetSum = 10
+        expected = [4, 6]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_3(self):
+        array = [4, 6, 1]
+        targetSum = 5
+        expected = [4, 1]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(sorted(expected), actual)
+
+    def test_case_4(self):
+        array = [4, 6, 1, -3]
+        targetSum = 3
+        expected = [-3, 6]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_5(self):
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        targetSum = 17
+        expected = [8, 9]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_6(self):
+        array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15]
+        targetSum = 18
+        expected = [3, 15]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_7(self):
+        array = [-7, -5, -3, -1, 0, 1, 3, 5, 7]
+        targetSum = -5
+        expected = [-5, 0]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_8(self):
+        array = [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47]
+        targetSum = 163
+        expected = [-47, 210]
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_9(self):
+        array = [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47]
+        targetSum = 164
+        expected = []
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_10(self):
+        array = [3, 5, -4, 8, 11, 1, -1, 6]
+        targetSum = 15
+        expected = []
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_11(self):
+        array = [14]
+        targetSum = 15
+        expected = []
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
+    def test_case_12(self):
+        array = [15]
+        targetSum = 15
+        expected = []
+        actual = twoNumberSum(array, targetSum)
+        self.assertEqual(expected, actual)
+
     
 if __name__ == "__main__":
     unittest.main()
