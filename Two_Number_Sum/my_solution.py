cec2a6b855cb302ad82acb5b8fa5fec7def5aa5b
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-29 13:09:31 -0500

    add solution 2

--- Two_Number_Sum/my_solution.py
+++ Two_Number_Sum/my_solution.py
@@ -1,9 +1,26 @@
+# solution 1
 def twoNumberSum(array, targetSum):
     array.sort()
     leftPoint = 0
     rightPoint = len(array) -1
 
     for i in range(len(array)):
+        sum = array[leftPoint] + array[rightPoint]
+        if sum < targetSum:
+            leftPoint += 1
+        elif sum > targetSum:
+            rightPoint -= 1
+        else:
+            return [array[leftPoint], array[rightPoint]]
+    return []
+
+# solution 2
+def twoNumberSum_sol2(array, targetSum):
+    array.sort()
+    leftPoint = 0
+    rightPoint = len(array) -1
+
+    while leftPoint < rightPoint:
         sum = array[leftPoint] + array[rightPoint]
         if sum < targetSum:
             leftPoint += 1
