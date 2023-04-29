140a5a9d3776d121e02f9c156966e6cd460d393a
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-29 12:45:15 -0500

    add solution

--- Two_Number_Sum/my_solution.py
+++ Two_Number_Sum/my_solution.py
@@ -1,2 +1,14 @@
 def twoNumberSum(array, targetSum):
-    return
+    array.sort()
+    leftPoint = 0
+    rightPoint = len(array) -1
+
+    for i in range(len(array)):
+        sum = array[leftPoint] + array[rightPoint]
+        if sum < targetSum:
+            leftPoint += 1
+        elif sum > targetSum:
+            rightPoint -= 1
+        else:
+            return [array[leftPoint], array[rightPoint]]
+    return []
