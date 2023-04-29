ab94558ea097c3abb6e1ef505fb307b15d37b6a4
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-04-29 12:08:51 -0500

    Change from assertTrue to assertEqual and test on case 1

--- Validate_Subsequence/Validate_Subsequence_test.py
+++ Validate_Subsequence/Validate_Subsequence_test.py
@@ -5,52 +5,54 @@ class TestSolution(unittest.TestCase):
     def test_case_1(self):
         array = [5, 1, 22, 25, 6, -1, 8, 10]
         sequence = [1, 6, -1, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+        expected = True
+        actual = isValidSubsequence(array, sequence)
+        self.assertEqual(expected, actual)
 
-    def test_case_2(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [5, 1, 22, 25, 6, -1, 8, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_2(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_3(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [5, 1, 22, 6, -1, 8, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_3(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [5, 1, 22, 6, -1, 8, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_4(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [22, 25, 6]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_4(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [22, 25, 6]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_5(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [1, 6, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_5(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [1, 6, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_6(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [5, 1, 22, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_6(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [5, 1, 22, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_7(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [5, -1, 8, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_7(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [5, -1, 8, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_8(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [25]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_8(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [25]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_9(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [1, 1, 1]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_9(self):
+    #     array = [1, 1, 1, 1, 1]
+    #     sequence = [1, 1, 1]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
-    def test_case_10(self):
-        array = [5, 1, 22, 25, 6, -1, 8, 10]
-        sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
-        self.assertTrue(isValidSubsequence(array, sequence))
+    # def test_case_10(self):
+    #     array = [5, 1, 22, 25, 6, -1, 8, 10]
+    #     sequence = [5, 1, 22, 22, 25, 6, -1, 8, 10]
+    #     self.assertEqual(isValidSubsequence(array, sequence))
 
  
 if __name__ == "__main__":
