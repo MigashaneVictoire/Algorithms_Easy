760a9434fc50d2d9dedf061ef9cb5274f4d841f4
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-05-01 10:36:38 -0500

    add solutions

--- Sorted_Squared_Array/solution.py
+++ Sorted_Squared_Array/solution.py
@@ -1,2 +1,26 @@
 def sortedSquareArray(array):
-    return
+    squared = []
+    for ele in array:
+        squared.append(abs(pow(ele,2)))
+    squared.sort()
+    return squared
+
+
+def sortedSquaredArray_sol2(array):
+    # create a list the length of the array, fill the list with zeros
+    zeros = [0 for _ in array]
+    leftPoint = 0
+    rightPoint = len(array) -1
+
+    for ele in range(len(array)):
+        squareLeft = pow(abs(array[leftPoint]),2)
+        squareRight = pow(abs(array[rightPoint]),2)
+    
+        if squareLeft <= squareRight:
+            zeros[ele] = squareLeft
+            leftPoint += 1
+        else:
+            zeros[ele] = squareRight
+            rightPoint -= 1
+    zeros.sort()
+    return zeros
