fd192714e24b524edec2d101d0e9e857aceb906f
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-05-01 22:03:06 -0500

    add solution function 2

--- Tournamet_Winner/solution.py
+++ Tournamet_Winner/solution.py
@@ -1,3 +1,4 @@
+# Solution 1
 def tournamentWinner(competitions, results):
     winners = []
     for key, value in enumerate(competitions):
@@ -6,3 +7,14 @@ def tournamentWinner(competitions, results):
         elif results[key] == 1:
             winners.append(value[0])
     return max(winners, key= winners.count)
+
+# Solution 2
+def tournamentWinner_sol2(competitions, results):
+    # Write your code here
+    countPoint = []
+    for key, value in enumerate(competitions):
+        if results[key] == 0:
+            countPoint.append(value[1])
+        elif results[key] == 1:
+            countPoint.append(value[0])
+    return max(countPoint, key=countPoint.count)
